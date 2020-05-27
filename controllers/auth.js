61ba55ae8b405e462967aa1fcffa56b6444f5760
Hamza Sahin <hsahin2@binghamton.edu>
2020-05-27 15:37:49 +0300

    Login Implemented

--- controllers/auth.js
+++ controllers/auth.js
@@ -51,12 +51,11 @@ exports.login = async(req, res) => {
 
         if (!email || !password) {
             return res.status(400).render('login', {
-                message: 'Please provide an email and password.'
+                message: 'Please enter an email and password.'
             });
         }
 
         db.query('SELECT * FROM users WHERE email = ? ', [email], async(error, results) => {
-            console.log(results);
             if (!results || !(await bcrypt.compare(password, results[0].password))) {
                 res.status(401).render('login', {
                     message: 'Email or password is incorrect'
@@ -64,9 +63,21 @@ exports.login = async(req, res) => {
             } else {
                 const user_ID = results[0].user_ID;
 
-                const token = jwt.sign({ id }, process.nextTick.JWT_SECRET, {
+                const token = jwt.sign({ user_ID }, process.env.JWT_SECRET, {
                     expiresIn: process.env.JWT_EXPIRES_IN
                 });
+
+                console.log("The token is: ", token);
+
+                const cookieOptions = {
+                    expires: new Date(
+                        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+                    ),
+                    httpOnly: true
+                };
+
+                res.cookie('jwt', token, cookieOptions);
+                res.status(200).redirect("/");
             }
         });
     } catch (error) {
